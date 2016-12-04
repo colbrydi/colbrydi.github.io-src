bd032ae529275742a10676503a6cbe6bbc447c16
DIrk Colbry <colbrydi@msu.edu>
2016-12-04 15:30:19 -0500

    I think I have jupyter notebooks working

--- pelicanconf.py
+++ pelicanconf.py
@@ -6,16 +6,21 @@ PROFILE_IMAGE_URL='http://colbrydi.github.io/images/Colbry_D.jpg'
 
 AUTHOR = 'Dirk Colbry'
 SITENAME = 'Dirk Colbry'
-SITEURL = ''
+#SITEURL = 'http://colbrydi.github.io'
+
+THEME = './theme/'
+
+TIMEZONE = 'EST'
+
+PLUGIN_PATHS=["./plugins"]
+PLUGINS=['ipynb.markup']
+MARKUP = { 'md', 'ipynb'}
 
 PATH = 'content'
 STATIC_PATHS = ['blog', 'images']
 ARTICLE_PATHS = ['blog']
 ARTICLE_SAVE_AS = '{date:%Y}/{slug}.html'
 ARTICLE_URL = '{date:%Y}/{slug}.html'
-
-THEME= './theme/'
-TIMEZONE = 'EST'
 
 DEFAULT_LANG = 'en'
 
@@ -25,6 +30,8 @@ CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
+
+MENUITEMS=[('Blog', '')]
 
 # Blogroll
 LINKS = (('Pelican', 'http://getpelican.com/'),
@@ -40,3 +47,35 @@ DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+
+
+# all the following settings are *optional*
+
+# all defaults to True.
+DISPLAY_HEADER = True
+DISPLAY_FOOTER = True
+DISPLAY_HOME   = True
+DISPLAY_MENU   = True
+
+# provided as examples, they make ‘clean’ urls. used by MENU_INTERNAL_PAGES.
+TAGS_URL           = 'tags'
+TAGS_SAVE_AS       = 'tags/index.html'
+AUTHORS_URL        = 'authors'
+AUTHORS_SAVE_AS    = 'authors/index.html'
+CATEGORIES_URL     = 'categories'
+CATEGORIES_SAVE_AS = 'categories/index.html'
+ARCHIVES_URL       = 'archives'
+ARCHIVES_SAVE_AS   = 'archives/index.html'
+
+# use those if you want pelican standard pages to appear in your menu
+#MENU_INTERNAL_PAGES = (
+#    ('Tags', TAGS_URL, TAGS_SAVE_AS),
+#    ('Authors', AUTHORS_URL, AUTHORS_SAVE_AS),
+#    ('Categories', CATEGORIES_URL, CATEGORIES_SAVE_AS),
+#    ('Archives', ARCHIVES_URL, ARCHIVES_SAVE_AS),
+#)
+# additional menu items
+#MENUITEMS = (
+#    ('GitHub', 'https://github.com/'),
+#    ('Linux Kernel', 'https://www.kernel.org/'),
+#)
